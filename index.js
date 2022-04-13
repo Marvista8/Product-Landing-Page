f191861e29efaf937d50fa15554230497d3124d9
Margaret Farmer <86688818+Marvista8@users.noreply.github.com>
2022-04-13 10:56:26 -0400

    adding and deleting some styliungs and js for slideshow.

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ let videoOpacity = () => {
  let videoShade = document.getElementById('video').style.backgroundColor = 'rgba(0,0,0,0.8)';
  console.log('videoShade: ', videoShade);
 };
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName('slides');
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 3000);
+}
